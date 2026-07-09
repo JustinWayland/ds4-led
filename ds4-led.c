7443fb7b50cc9bd81d4545cb56815d8a86350142
JustinWayland <justintwayland+github@gmail.com>
2026-07-08 20:54:06 -0400

    Replace executable with source code.

---
+++ ds4-led.c
@@ -0,0 +1,41 @@
+#include <fcntl.h>
+#include <linux/hidraw.h>
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+
+int main(int argc, char** argv) {
+  char path[14];
+  uint8_t report[32];
+  if (argc <= 1) {
+    printf("Color must be supplied to this program.\n");
+    return 1;
+  }
+  report[0] = 0x05;
+  report[1] = 0x07;
+  sscanf(argv[1], "#%2x%2x%2x",&report[6],&report[7],&report[8]);
+  int i, fd;
+  for (i = 0; i < 8; i++) {
+    sprintf(path, "/dev/hidraw%d", i);
+    fd = open(path, O_RDWR | O_NONBLOCK);
+    struct hidraw_devinfo info;
+
+    if (ioctl(fd, HIDIOCGRAWINFO, &info) >= 0) {
+      if (info.vendor == 0x054c && (info.product == 0x05c4 || info.product == 0x09cc)) {
+          break;
+      }
+    }
+    close(fd);
+  }
+  if (i == 8) {
+    printf("No Dualshock 4 Controllers connected.");
+    return -1;
+  }
+  int res = ioctl(fd, HIDIOCSOUTPUT(sizeof(report)), report);
+  if (res < 0) {
+    printf("Failed to change Dualshock 4 LED color");
+    return res;
+  }
+  return 0;
+}
