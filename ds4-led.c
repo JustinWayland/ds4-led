ecfbfb7e0c9e9f88162fdee8cda691706c82ff0c
JustinWayland <justintwayland+github@gmail.com>
2026-07-13 22:10:00 -0400

    Dynamically count hidraw devices.
    Also change printf to perror.

--- ds4-led.c
+++ ds4-led.c
@@ -4,24 +4,40 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <dirent.h>
 #include <unistd.h>
 
 int main(int argc, char** argv) {
   char path[14];
   uint8_t report[32];
+  DIR* dir;
+  struct dirent* entry;
+  int count = 0;
+  // Initialization
   memset(report, 0, 32);
   if (argc <= 1) {
-    printf("Color must be supplied to this program.\n");
+    perror("Color must be supplied to this program.\n");
     return 1;
   }
   report[0] = 0x05;
   report[1] = 0x07;
   if (sscanf(argv[1], "#%2x%2x%2x",&report[6],&report[7],&report[8]) != 3) {
-    printf("argv[1] not in the hex string color form.\n");
+    perror("argv[1] not in the hex string color form.\n");
     return -1;
   }
+  // Count hidraw devices
+  dir = opendir("/sys/class/hidraw");
+  if (!dir) {
+    perror("Unable to open /sys/class/hidraw");
+    return -1;
+  }
+  while ((entry = readdir(dir)) != NULL) {
+    if (strncmp(entry->d_name, "hidraw", 6) == 0)
+      count++;
+  }
+  closedir(dir);
   int i, fd;
-  for (i = 0; i < 8; i++) {
+  for (i = 0; i < count; i++) {
     sprintf(path, "/dev/hidraw%d", i);
     fd = open(path, O_RDWR | O_NONBLOCK);
     struct hidraw_devinfo info;
@@ -33,13 +49,13 @@ int main(int argc, char** argv) {
     }
     close(fd);
   }
-  if (i == 8) {
-    printf("No Dualshock 4 Controllers connected.\n");
+  if (i == count) {
+    perror("No Dualshock 4 Controllers connected.\n");
     return -1;
   }
   int res = ioctl(fd, HIDIOCSOUTPUT(sizeof(report)), report);
   if (res < 0) {
-    printf("Failed to change Dualshock 4 LED color\n");
+    perror("Failed to change Dualshock 4 LED color\n");
     return res;
   }
   return 0;
